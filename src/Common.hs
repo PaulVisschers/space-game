cd8f2388d215002132e05aa3a7f1bae889b5e3b6
Paul Visschers <github@paulvisschers.net>
2012-12-09 22:12:32 +0100

    Moved ray-tracing to common and renamed stores to datastores

--- src/Common.hs
+++ src/Common.hs
@@ -9,9 +9,9 @@ import Data.Vector
 
 newtype Ref a = Ref Int deriving (Eq, Ord, Enum, Show, Read)
 
-type Store a = Map.Map (Ref a) a
+type DataStore a = Map.Map (Ref a) a
 
-ref :: Ref a -> Store a :-> a
+ref :: Ref a -> DataStore a :-> a
 ref ix = lens (Map.! ix) (\x m -> Map.insert ix x m)
 
 -- * Data
@@ -31,7 +31,7 @@ data Combination = Combination {
   } deriving (Show, Read)
 
 data Scene = Scene {
-  _players :: Store Player
+  _players :: DataStore Player
   } deriving (Show, Read)
 
 emptyScene = Scene Map.empty
@@ -45,3 +45,38 @@ data WalkingKey = WalkForward | WalkBackward | WalkRight | WalkLeft | WalkUp | W
 
 timeDiff :: Clock.UTCTime -> Clock.UTCTime -> Double
 timeDiff x y = fromRational $ toRational $ Clock.diffUTCTime x y
+
+-- Ray tracing
+data RayTraceResult f a = Miss | Inside | Hit (a, f a) (a, f a) deriving (Eq, Ord, Show)
+
+rayTraceAABB :: (DivisionRing a, Nat n, Ord a) => Vector n a -> Vector n a -> Vector2 (Vector n a) -> RayTraceResult (Vector n) a
+rayTraceAABB pos dir (Cons low (Cons high Nil))
+  | any (== Miss) dims = Miss
+  | all (== Inside) dims = Inside
+  | fst near > fst far = Miss
+  | fst far < zero = Miss
+  | otherwise = Hit near far where
+    dims = zipWith5 rayTraceAABB1D pos dir low high one
+    (nears, fars) = Prelude.unzip . hits . toList $ dims
+    near = maximum nears
+    far = minimum fars
+
+rayTraceAABB1D :: (DivisionRing a, Ord a) => a -> a -> a -> a -> Vector n a -> RayTraceResult (Vector n) a
+rayTraceAABB1D pos dir low high normal 
+  | dir == zero = if pos < low || pos > high then Miss else Inside
+  | otherwise = Hit near far where
+    distX = (low - pos) / dir
+    distY = (high - pos) / dir
+    x = (distX, fmap negate normal)
+    y = (distY, normal)
+    near = if distX <= distY then x else y
+    far = if distX > distY then x else y
+
+hits :: [RayTraceResult f a] -> [((a, f a), (a, f a))]
+hits [] = []
+hits (Hit x y:xs) = (x, y) : hits xs
+hits (_:xs) = hits xs
+
+isHit :: RayTraceResult f a -> Bool
+isHit (Hit _ _) = True
+isHit _ = False
